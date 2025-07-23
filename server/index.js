661d0c5087f053d75ea5a1d9ab7482258ae31a6a
Sehajdeep Singh <sehajdeepp17@gmail.com>
2025-07-23 18:19:07 +0530

    version fix

--- server/index.js
+++ server/index.js
@@ -63,13 +63,6 @@ io.on("connection", (socket) => {
 });
 
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
 const PORT = process.env.PORT || 3000;
 
 async function main() {
